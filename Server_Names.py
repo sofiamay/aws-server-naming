6fd5ab8ed7dea93812ae7e0cbfb68e5e0df91260
Sofia Berlin <sofiamberlin@gmail.com>
2019-04-29 15:31:43 -0700

    create functions corresponding to each suffix

--- Server_Names.py
+++ Server_Names.py
@@ -19,51 +19,52 @@ def read_file(filename, func=None):
             array.append(line)
         return array
 
-# boolean: returns true if a word matches at least one regex in a list
-def matches_regexes(word, regexes):
-    for regex in regexes:
-        if regex.match(word):
-            return True
-    return False
+# returns a the suffix as a string if a word matches at least one regex in a list
+# each suffix has the form [regex, string]
+def matches_regexes(word, suffixes):
+    for suffix in suffixes:
+        if suffix[0].match(word):
+            return suffix[1]
+    return ""
 
-# Each of these functions contains instructions for how to deal with a different suffix
+# # Each of these functions contains instructions for how to deal with a different suffix
 
-def able(word):
+def able(word, server_names):
     # Delete suffix, if result is already in the list, return that word. Else return full word
     if word[0:-4] in server_names:
         return word[0:-4]
     else:
         return word
 
-def ed(word):
+def ed(word, server_names):
     # if root word is in list, return that word, else return full word
     if word[0:-2] in server_names:
         return word[0:-2]
     else:
         return word
 
-def er(word):
+def er(word, server_names):
     # if root word is in list, return it. Else add full word to list
     if word[0:-2] in server_names:
         return word[0:-2]
     else: return word
 
-def es(word):
+def es(word, server_names):
     vowels = ["a","e","i","o","u"]
     # ends in vowel + consonant, remove the "s"
     if (word[-3] not in vowels) and (word[-4] in vowels):
         return word[0:-1]
     # ends in "ies," replace with "y"
-    else if word[-3] == "i":
+    elif word[-3] == "i":
         return word[0:-3] + "y"
     # ends in "ves," replace with "f"
-    else if word[-3] == "v":
-        return = word[0:-3] + f
+    elif word[-3] == "v":
+        return word[0:-3] + "f"
     # else, remove "es"
     else:
         word = word[0:-2]
 
-def est(word):
+def est(word, server_names):
     # if ends in "iest" transform to "y" and return
     if word[-4] == "i":
         return word[0:-4] + "y"
@@ -73,33 +74,123 @@ def est(word):
     else:
         return word
 
+def ification(word, server_names):
+    # ignore word
+    return ""
+
+def ization(word, server_names):
+    return ""
+
+def tional(word, server_names):
+    return ""
+
+def tioning(word, server_names):
+    return ""
+
+def ation(word, server_names):
+    return ""
+
+def tion(word, server_names):
+    # delete "ion." If root not a word, return full word
+    if word[0:-3] in server_names:
+        return word[0:-3]
+    else:
+        return word
+
+def fully(word, server_names):
+    return ""
+
+def ify(word, server_names):
+    return ""
+
+def ian(word, server_names):
+    return ""
+
+def ic(word, server_names):
+    # if root is a word, return it. Else return full word
+    if word[0:-2] in server_names:
+        return word[0:-2]
+    else:
+        return word
+
+def ing(word, server_names):
+    # remove suffix
+    return word[0:-3]
+
+def ion(word, server_names):
+    # replace suffix with "e"
+    return word[0:-3] + "e"
+
+def ism(word, server_names):
+    return ""
+
+def ist(word, server_names):
+    return ""
+
+def ity(word, server_names):
+    return ""
+
+def logy(word, server_names):
+    # if root word is in list, return it. Else add full word to list
+    if word[0:-4] in server_names:
+        return word[0:-4]
+    else:
+        return word
+
+def ally(word, server_names):
+    return ""
+
+def ly(word, server_names):
+    # if root word is in list, return it. Esle add full word to list
+    if word[0:-2] in server_names:
+        return word[0:-2]
+
+    else:
+        return word
+
+def ment(word, server_names):
+    return ""
+
+def ness(word, server_names):
+    return ""
+
+def ship(word, server_names):
+    # if root word is in list, return it. Esle add full word to list
+    if word[0:-4] in server_names:
+        return word[0:-4]
+    else:
+        return word
 
 
-# global variables
-server_names = {}
 
 
 
-# def main():
-#     #read file
-#     word_list = read_file("data/wordList")
-#     # generates a list of suffixes and their lengths. E.g. [ ['ing',3], ['ed',2],...]
-#     suffixes = read_file("data/suffixes", lambda x: re.compile(r'\w*%s\b' % x) )
+def main():
+    #read file
+    word_list = read_file("data/wordList")
+    # generates a list of suffixes, each a tuple of the form [regex, string]
+    suffixes = read_file("data/suffixes", lambda x: [re.compile(r'\w*%s\b' % x), x] )
 
-#     # stem words and remove stopwords
-#     stop_words = set(stopwords.words("english"))
-#     server_names = []
+    # stem words and remove stopwords
+    stop_words = set(stopwords.words("english"))
+    server_names = set()
 
-#     for word in word_list:
-#         if word not in stop_words:
-#             if not matches_regexes(word, suffixes):
-#                 server_names.append(word)
+    for word in word_list:
+        if len(word) < 12 and word not in stop_words:
+            words_suffix = matches_regexes(word, suffixes)
+            if not words_suffix:
+                # word has no suffix. Append as is to the final list
+                server_names.add(word)
+            else:
+                # word has suffix. Process before appending
+                final_word = globals()[words_suffix](word, server_names)
 
-#     with open('data/final_word_list.txt', 'w') as f:
-#         for item in server_names:
-#             f.write("%s\n" % item)
 
-#     return server_names
+    with open('data/final_word_list.txt', 'w') as f:
+        for item in server_names:
+            f.write("%s\n" % item)
+
+    return server_names
 
 
 if __name__ == "__main__":
