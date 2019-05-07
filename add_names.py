8dde091f804b4aae212c71df2e754a26cba7b90c
Sofia Berlin <sofiamberlin@gmail.com>
2019-05-06 18:22:46 -0700

    fix typo

--- add_names.py
+++ add_names.py
@@ -5,7 +5,7 @@ from tqdm import tqdm
 dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 table = dynamodb.Table('ServerNames')
 
-# Use in AWS Cloud9 with word_list.csv and ServNames table in DynamoDB
+# Use in AWS Cloud9 with word_list.csv and ServerNames table in DynamoDB
 
 # Open CSV
 with open('word_list.csv') as csvfile:
