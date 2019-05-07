1256daaed06d77501df9f29a7c55dc4796f983ef
Sofia Berlin <sofiamberlin@gmail.com>
2019-05-07 14:31:40 -0700

    Create a function that adds a server name entry to the dynamoDB

---
+++ add_server_name.py
@@ -0,0 +1,23 @@
+from __future__ import print_function # Python 2/3 compatibility
+import boto3
+import json
+
+#Adds a name to the table
+
+
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+table = dynamodb.Table('ServerNames')
+
+
+# Edit this to be the name of the terminated server
+name = "test"
+
+
+response = table.put_item(
+   Item={
+        'Age': name,
+    }
+)
+
+print("PutItem succeeded:")
+print(json.dumps(response, indent=4))
