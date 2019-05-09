1d2421ef99871e4d95121e18f823bf6b9aab869e
Sofia Berlin <sofiamberlin@gmail.com>
2019-05-09 12:00:28 -0700

    Added error handling and comments to get_name_and_delete

--- get_name_and_delete.py
+++ get_name_and_delete.py
@@ -1,34 +1,41 @@
 import json
 import boto3
 from botocore.exceptions import ClientError
+#When starting a new EC2 istnace: a
+# ) retrieve a name from the database b) assign it to the new EC2 instance c) delete that name from the database
 
 client = boto3.client('ec2')
 dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 table = dynamodb.Table('ServerNames')
 
+# a) retrieve a name from the database
 response = table.scan(Limit=1)   
-server = response['Items'][0]
-
-# Assign the server name to the new server:
 server_name = response['Items'][0]["Name"]
 
 def lambda_handler(event, context):
+    # b) Assign it to a new EC2 instance
     instance_id = event['detail']['instance-id']
-    # Retrieve a name from the database
-    
-    
-    response = client.create_tags(
-    DryRun=False,
-    Resources=[
-        instance_id,
-    ],
-    Tags=[
+    tags=[
         {
             'Key': 'Name',
             'Value': server_name
         },
     ]
-)
+    
+    try:
+        client.create_tags(DryRun=False, Resources=[instance_id],Tags=tags)
+    except ClientError as e:
+            if 'DryRunOperation' not in str(e):
+                raise
+
+    try:
+        response = client.create_tags(DryRun=False,Resources=[instance_id],Tags=tags)
+        print("Assigned a name to server successfully: ", response)
+    except ClientError as e:
+        print("Error: ", e)
+
+
+    # C) delete server_name from the database
     deleted_item = table.delete_item(
     Key={
         'Name': server_name
